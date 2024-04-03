3bbf57b8e56a3f2eb8b4694e6190200629c7925e
Bin <9633157+web1n@users.noreply.github.com>
2024-04-03 20:25:21 +0800

    Initial commit

---
+++ tempest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+EXTRA_ARGS=()
+
+# Borrowed from: https://github.com/flathub/md.obsidian.Obsidian/blob/5312e50f13f48efccd60a90f282b92b38bcec9a1/obsidian.sh#L20
+WL_DISPLAY="${WAYLAND_DISPLAY:-"wayland-0"}"
+# Some compositors a real path a instead of a symlink for WAYLAND_DISPLAY:
+# https://github.com/flathub/md.obsidian.Obsidian/issues/284
+if [[ -e "${XDG_RUNTIME_DIR}/${WL_DISPLAY}" || -e "/${WL_DISPLAY}" ]]; then
+    echo "Debug: Enabling Wayland backend"
+    EXTRA_ARGS+=(
+        --ozone-platform-hint=auto
+        --enable-features=WaylandWindowDecorations
+        --enable-wayland-ime
+    )
+    if [[ -c /dev/nvidia0 ]]; then
+        echo "Debug: Detecting Nvidia GPU. disabling GPU sandbox."
+        EXTRA_ARGS+=(
+            --disable-gpu-sandbox
+        )
+    fi
+fi
+
+exec zypak-wrapper.sh /app/tempest/Tempest "${EXTRA_ARGS[@]}" "$@"
